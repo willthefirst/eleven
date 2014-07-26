01a64965ae94ba3ff1af62b34d8dd195a4565dff
Will Moritz <will@crowdfavorite.com>
2014-07-26 18:13:22 -0400

    integrated webaudio

--- js/main.js
+++ js/main.js
@@ -5,12 +5,48 @@ var KNOB = {
 	numOfStems: 0,
 	stemRanges: [],
 	stemRangeLength: 0,
+	stemAudio : [],
+	context: {},
 
 	init: function() {
 		this.getWindowHeight();
 		this.countStems();
 		this.trackMouse();
-		this.playAllTracks();
+		this.createAudioContext();
+	},
+
+	createAudioContext : function() {
+
+		var context;
+		var bufferLoader;
+		var stems = this.getAllTracks();
+		var self = this;
+
+		// Fix up prefixing
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+		context = new AudioContext();
+
+		bufferLoader = new BufferLoader(
+			context,
+			stems,
+			finishedLoading
+		);
+
+		bufferLoader.load();
+
+		function finishedLoading(bufferList) {
+			console.log('loaded baby');
+			var source;
+			for (var i = 0; i < self.numOfStems; i++) {
+				source = context.createBufferSource();
+				source.buffer = bufferList[i];
+				source.connect(context.destination);
+				self.stemAudio.push(source);
+			}
+			for (var i = 0; i < self.stemAudio.length; i++) {
+				self.stemAudio[i].start(0);
+			}
+		}
 	},
 
 	getWindowHeight: function() {
@@ -33,10 +69,13 @@ var KNOB = {
 		});
 	},
 
-	playAllTracks: function() {
+	getAllTracks: function() {
+		var array = [];
 		for (var i = 0; i < this.numOfStems; i++) {
-			$('.stems').children()[i].play();
+			var child = $('.stems').children("audio")[i];
+			array.push(child.src);
 		}
+		return array;
 	},
 
 	updateDOM: function(e) {
@@ -74,3 +113,52 @@ var KNOB = {
 $( document ).ready(function() {
 	KNOB.init();
 });
+
+
+// Make the Buffer Loader
+function BufferLoader(context, urlList, callback) {
+  this.context = context;
+  this.urlList = urlList;
+  this.onload = callback;
+  this.bufferList = new Array();
+  this.loadCount = 0;
+}
+
+BufferLoader.prototype.loadBuffer = function(url, index) {
+  // Load buffer asynchronously
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
+
+  var loader = this;
+
+  request.onload = function() {
+    // Asynchronously decode the audio file data in request.response
+    loader.context.decodeAudioData(
+      request.response,
+      function(buffer) {
+        if (!buffer) {
+          alert('error decoding file data: ' + url);
+          return;
+        }
+        loader.bufferList[index] = buffer;
+        if (++loader.loadCount == loader.urlList.length)
+          loader.onload(loader.bufferList);
+      },
+      function(error) {
+        console.error('decodeAudioData error', error);
+      }
+    );
+  }
+
+  request.onerror = function() {
+    alert('BufferLoader: XHR error');
+  }
+
+  request.send();
+}
+
+BufferLoader.prototype.load = function() {
+  for (var i = 0; i < this.urlList.length; ++i)
+  this.loadBuffer(this.urlList[i], i);
+}
