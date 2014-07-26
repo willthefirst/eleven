8a73f9b255e47d54106c80e0f19ff39842cce566
Will Moritz <will@crowdfavorite.com>
2014-07-26 16:44:51 -0400

    set up stem ranges

--- js/main.js
+++ js/main.js
@@ -1,33 +1,45 @@
 var KNOB = {
 
-	config: {
-
-	},
-
 	height: 0,
 	mouseY: 0,
+	numOfStems: 0,
+	stemRanges: [],
 
 	init: function() {
 		this.getWindowHeight();
+		this.countStems();
 		this.trackMouse();
-		this.layoutStems();
 	},
 
 	getWindowHeight: function() {
 		this.height = $(window).height();
 	},
 
+	countStems: function() {
+		this.numOfStems = $('.stems').children().length;
+		for (var i = 0; i < this.numOfStems; i++) {
+			this.stemRanges.push(100/this.numOfStems * (i+1));
+		}
+	},
+
 	trackMouse: function() {
 		var self = this;
 		$('.wrapper').on('mousemove', function(e) {
-			self.mouseY = 1 - (e.pageY / self.height);
-			$('.mouse-y').html(self.mouseY);
+			self.updateDOM(e);
+			self.updateAudio(e);
 		});
 	},
 
-	layoutStems: function() {
+	updateDOM: function(e) {
+		this.mouseY = Math.round(100 * (1 - (e.pageY / this.height)));
+		$('.mouse-y').html(this.mouseY);
+	},
 
+	updateAudio: function(e) {
+		// Current mouse percentage tells us
+		// console.log(e);
 	}
+
 
 };
 
